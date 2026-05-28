97d89cef2784824ef2f977a88e09a7abed0af57a
Francis <francisli0722@gmail.com>
2026-05-29 02:35:24 +1000

    add support for other main language

--- src/core/parser-loader.ts
+++ src/core/parser-loader.ts
@@ -1,16 +1,25 @@
 import Parser from "web-tree-sitter";
 import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const cache = new Map<string, any>();
 
 export async function loadLanguage(lang: string) {
-  if (cache.has(lang)) return cache.get(lang);
+    if (cache.has(lang)) return cache.get(lang);
 
-  const wasmPath = `./tree-sitter-parser/${lang}.wasm`;
-  const buffer = fs.readFileSync(wasmPath);
+    const wasmPath = path.join(
+        __dirname,
+        "../tree-sitter-parser",
+        `${lang}.wasm`
+    );
+    const buffer = fs.readFileSync(wasmPath);
 
-  const language = await Parser.Language.load(buffer);
-  cache.set(lang, language);
+    const language = await Parser.Language.load(buffer);
+    cache.set(lang, language);
 
-  return language;
+    return language;
 }
